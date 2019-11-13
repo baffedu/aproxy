aea3757ed1ad0471815b2fe973d1742f3a1d7d52
zhouzihao <zhouzihao@baffedu.com>
2019-11-13 10:28:52 +0800

    dev-添加一般php项目

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # aproxy
+
+FPM_HOST 设置fpm服务的地址
+
+TYPE
+- laravel laravel的php项目启动在public文件夹下
+- general 普通php项目
+- 'default' 其他值是前端项目
+
+注意.htanginx文件必选存在
--- run.sh
+++ run.sh
@@ -6,8 +6,14 @@ if [ $TYPE == "laravel" ]
 then
     sed -i "s/{fpm_host}/"$FPM_HOST"/g"  /etc/nginx/conf.d/laravel.conf
 	rm /etc/nginx/conf.d/front.conf
+	rm /etc/nginx/conf.d/generalphp.conf
+else if [ $TYPE == "general"]
+then
+	rm /etc/nginx/conf.d/front.conf
+	rm /etc/nginx/conf.d/laravel.conf
 else 
 	rm /etc/nginx/conf.d/laravel.conf
+	rm /etc/nginx/conf.d/generalphp.conf
 fi
 
 nginx -g 'daemon off;'
